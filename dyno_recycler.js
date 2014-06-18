dc117236ec210de5d7106b359d28a276f6697cd1
thomas cool <thomas.cool@gmail.com>
2014-06-18 06:46:06 -0700

    enable H12 on recycler

--- dyno_recycler.js
+++ dyno_recycler.js
@@ -170,3 +170,4 @@ var recycler=function(timeoutAllowed, keyWords, ignore) {
 };
 
 recycler(2, 'Error%20R14');  // Error R14 (Memory quota exceeded)
+recycler(3, 'H12', 'path=/utils/bitly/shorten');  // error code=H12 desc="Request timeout"
